ba98e5400a2a93b1efc77e887ed8fb28bae83cf9
md-masum <masum.nextu@gmail.com>
2023-05-19 21:31:46 +0600

    deleted function confirmed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,6 +39,13 @@ async function run() {
     app.get('/toys', async(req,res) => {
         const cursor = toyCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.delete('/toys/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.deleteOne(query);
         res.send(result);
     })
 
