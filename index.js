492bb7d1d59b9bc946ffbed864a203f6b8e5eb90
md-masum <masum.nextu@gmail.com>
2023-05-21 23:38:05 +0600

    vercel problem solveing

--- index.js
+++ index.js
@@ -19,13 +19,23 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser:true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
+
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+
+    client.connect((error)=>{
+        if(error){
+            console.log(error)
+            return;
+    }
+});
 
     const toyCollection = client.db('toyDB').collection('toys');
 
